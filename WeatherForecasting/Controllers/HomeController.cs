f4c1f1769fda725c604683c00f66bd01556d0a56
kalpani <dinushikacpk@gmail.com>
2021-05-02 00:31:25 -0700

    init

---
+++ WeatherForecasting/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Linq;
+using WeatherForecasting.Models;
+
+namespace WeatherForecasting.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SqlConnection _connection { get; set; }
+        public HomeController()
+        {
+            _connection = new SqlConnector().GetConnection();
+        }
+        public ViewResult Index()
+        {
+            var sql = "SELECT Id,Name FROM Locations";
+            var locations = _connection.Query<Location>(sql).ToList();
+            return View(locations);
+        }
+
+        [HttpGet]
+        public PartialViewResult GetForecastData(int locationId)
+        {
+            var sql = $"SELECT Id,ForecastDate,WeatherType,MinTemp,MaxTemp FROM forecast " +
+                $"WHERE LocationId = {locationId} AND forecastDate > '{DateTime.Today}'";
+            var forecast = _connection.Query<Forecast>(sql).ToList();
+            return PartialView("GetForecastData", forecast);
+        }
+    }
+}
+
---
+++ WeatherForecasting/Models/SqlConnector.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace WeatherForecasting.Models
+{
+    public class SqlConnector
+    {
+        private SqlConnection _connection { get; set; }
+        public SqlConnector()
+        {
+            _connection = new SqlConnection(GlobalConfig.ConnectionString);
+        }
+        public SqlConnection GetConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Closed)
+            {
+                _connection = new SqlConnection(GlobalConfig.ConnectionString);
+            }
+            return _connection;
+        }
+
+    }
+}
